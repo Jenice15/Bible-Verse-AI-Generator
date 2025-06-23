5ed2cc0f2e346280ba753cf94668d31ee7913fd8
Jenice15 <jenicemartin15@gmail.com>
2025-06-23 12:13:05 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -98,7 +98,8 @@ async function searchVerse(event) {
         searchBtn.disabled = false;
         searchInput.value = ''; // Reset input
         searchInput.blur(); // Close keyboard on mobile
-        history.go(0.5);
+        // history.go(0.30);
+        windows.location.reload();
 
         // Re-add event listener to prevent issues with the async call
         let form = document.querySelector('#verse-generator-form');
